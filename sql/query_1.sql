11ed674aa71682f647d68b12cacf572b568c5722
Dmytro-Ostrenko <142011911+Dmytro-Ostrenko@users.noreply.github.com>
2024-02-12 19:08:27 +0200

    Add files via upload

---
+++ sql/query_1.sql
@@ -0,0 +1,6 @@
+SELECT Students.name, AVG(Grades.grade) AS average_grade
+FROM Students
+JOIN Grades ON Students.student_id = Grades.student_id
+GROUP BY Students.student_id
+ORDER BY average_grade DESC
+LIMIT 5;
